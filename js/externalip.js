4a00305b8691b5f9f1afe484eb6de04a9498a493
Mark Lintern <mlintern@compendium.com>
2013-03-13 22:46:06 -0400

    epic fail

--- js/externalip.js
+++ js/externalip.js
@@ -1,6 +1,15 @@
 $.get('http://icanhazip.com', function(data,status,xhr){
-	console.log(data);
-	console.log(status);
-	console.log(xhr);
-	$('#External-IP').html(data);
+	//console.log(data);
+	//console.log(status);
+	//console.log(xhr);
+	//$('#External-IP').html(data);
 },"html");
+
+$.ajax({
+  type:'GET',
+  url: "http://icanhazip.com",
+  headers: {"Access-Control-Allow-Origin":"http://icanhazip.com"}
+}).done(function(data){
+    console.log(data);
+    $('#External-IP').html(data);
+});
