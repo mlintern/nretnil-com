b857c5c66ba72c159dbf738a6f18ac187f61fbd1
Mark Lintern <mlintern@compendium.com>
2013-03-14 19:56:22 -0400

    added url variables

--- php/dns.php
+++ php/dns.php
@@ -3,16 +3,21 @@
 //$host = gethostbyaddr("69.174.114.71");
 //echo "marklintern.com has the IP $ip, which reverses to $host";
 
+$ipaddr = $_GET['ipaddr'];
+$hostname = $_GET['hostname'];
+
 function get_dns_ip($hostname){
    $ip = gethostbyname($hostname);
-   return($ip);
+   echo $ip;
+   //return($ip);
 }
 
 function get_dns_hostname($ipaddr){
    $host = gethostbyaddr($ipaddr);
-   return($host);
+   echo $host;
+   //return($host);
 }
 
-echo get_dns_ip("nretnil.com");
-echo get_dns_hostname("192.81.212.27");
+//echo get_dns_ip("nretnil.com");
+//echo get_dns_hostname("192.81.212.27");
 ?>
