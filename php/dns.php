50f762f54fa0caaf3d1df14584f555c0d955b447
root <root@MLCLOUD.(none)>
2013-03-15 00:14:20 +0000

    dns php updates

--- php/dns.php
+++ php/dns.php
@@ -3,21 +3,30 @@
 //$host = gethostbyaddr("69.174.114.71");
 //echo "marklintern.com has the IP $ip, which reverses to $host";
 
-$ipaddr = $_GET['ipaddr'];
+//$ipaddr = $_GET['ipaddr'];
+
+if ($_GET['hostname'] != null){
 $hostname = $_GET['hostname'];
+get_dns_ip($hostname);
+}
+
+if ($_GET['ipaddr'] != null){
+$ipaddr = $_GET['ipaddr'];
+get_dns_hostname($ipaddr);
+}
 
 function get_dns_ip($hostname){
-   $ip = gethostbyname($hostname);
-   echo $ip;
+   //$ip = gethostbyname($hostname);
    //return($ip);
+   $ips = gethostbynamel($hostname);
+   foreach ($ips as $ip => $value){
+      echo $value . "<br>";
+   }
 }
 
-function get_dns_hostname($ipaddr){
-   $host = gethostbyaddr($ipaddr);
-   echo $host;
+function get_dns_hostname($ip){
+   $host = gethostbyaddr($ip);
    //return($host);
+   echo $host;
 }
-
-//echo get_dns_ip("nretnil.com");
-//echo get_dns_hostname("192.81.212.27");
 ?>
