625c9b7824b9b3440990bef19db76e0c4b1671b6
Mark Lintern <mlintern@compendium.com>
2013-03-14 20:32:22 -0400

    dns javascript functions

---
+++ js/dns.js
@@ -0,0 +1,37 @@
+$.get('/php/ip.php', function(data,status,xhr){
+	console.log(data);
+	console.log(status);
+	console.log(xhr);
+	$('#External-IP').html(data);
+},"html");
+
+function iplookup(){
+	var ipaddr = $("#ip-info").val();
+	
+	var url = '/php/dns.php?ipaddr=' + ipaddr;
+	
+	$.get(url, function(data,status,xhr){
+		console.log(data);
+		$('#ip-info-div').html(data);
+	},"html");
+}
+
+function hostnamelookup(){
+	var hostname = $("#hostname-info").val();
+	
+	var url = '/php/dns.php?hostname=' + hostname;
+	
+	$.get(url, function(data,status,xhr){
+		console.log(data);
+		$('#hostname-info-div').html(data);
+	},"html");
+}
+
+/*$.ajax({
+  type:'GET',
+  url: "http://icanhazip.com",
+  headers: {"Access-Control-Allow-Origin":"http://icanhazip.com"}
+}).done(function(data){
+    console.log(data);
+    $('#External-IP').html(data);
+});*/
