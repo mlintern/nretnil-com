f039f047839ebab198291a423767c12cd08449bc
Mark Lintern <mlintern@compendium.com>
2013-03-18 19:21:31 -0400

    localized jquery and styled cron generator

--- js/cron.js
+++ js/cron.js
@@ -9,33 +9,27 @@ function generatecron(){
 	
 	if (min == 'custom'){
 		mins = $("select[name='custminutes[]']").val();
-		console.log(mins);
 		min = mins.join(',');
 	}
 	if (hour == 'custom'){
 		hrs = $("select[name='custhours[]']").val();
-		console.log(hrs);
 		hour = hrs.join(',');
 	}
 	if (day == 'custom'){
 		days = $("select[name='custdays[]']").val();
-		console.log(days);
 		day = days.join(',');
 	}
 	if (month == 'custom'){
 		mons = $("select[name='custmonths[]']").val();
-		console.log(mons);
 		month = mons.join(',');
 	}
 	if (weekday == 'custom'){
 		wds = $("select[name='custweekdays[]']").val();
-		console.log(wds);
 		weekday = wds.join(',');
 	}
 
 	var full = min+" "+hour+" "+day+" "+month+" "+weekday+" "+command;
-	
 	console.log(full);
-	
+	window-alert(full);
 	$("#cron-code").text(full);
 }
