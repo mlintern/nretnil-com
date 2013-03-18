3f85b60bc305a32b8d0fd39b0aa491eb986cf850
root <root@MLCLOUD.(none)>
2013-03-18 22:03:22 +0000

    updates

--- php/traceroute.php
+++ php/traceroute.php
@@ -3,45 +3,50 @@
     define ("SOL_IP", 0);
     define ("IP_TTL", 2);    // On OSX, use '4' instead of '2'.
 
-    $dest_url = "www.google.com";   // Fill in your own URL here, or use $argv[1] to fetch from commandline.
-    $maximum_hops = 30;
+    $dest_url = "www.yahoo.com";   // Fill in your own URL here, or use $argv[1] to fetch from commandline.
+    $maximum_hops = 2;
     $port = 33434;  // Standard port that traceroute programs use. Could be anything actually.
 
     // Get IP from URL
-    $dest_addr = gethostbyname ($dest_url);
-    print "Tracerouting to destination: $dest_addr\n";
+    $dest_addr = gethostbyname($dest_url);
+    print "Tracerouting to destination: $dest_addr </br>";
 
     $ttl = 1;
     while ($ttl < $maximum_hops) {
         // Create ICMP and UDP sockets
-        $recv_socket = socket_create (AF_INET, SOCK_RAW, getprotobyname ('icmp'));
-        $send_socket = socket_create (AF_INET, SOCK_DGRAM, getprotobyname ('udp'));
+	$protocol = 'icmp';
+	$get_prot = getprotobyname($protocol);
+        $recv_socket = socket_create(AF_INET, SOCK_RAW, $get_prot);
+	$protocol = 'udp';
+	$get_prot = getprotobyname($protocol);
+        $send_socket = socket_create(AF_INET, SOCK_DGRAM, $get_prot);
 
         // Set TTL to current lifetime
-        socket_set_option ($send_socket, SOL_IP, IP_TTL, $ttl);
+        socket_set_option($send_socket, SOL_IP, IP_TTL, $ttl);
 
         // Bind receiving ICMP socket to default IP (no port needed since it's ICMP)
-        socket_bind ($recv_socket, 0, 0);
+        socket_bind($recv_socket, 0, 0);
 
         // Save the current time for roundtrip calculation
-        $t1 = microtime (true);
+        $t1 = microtime(true);
 
         // Send a zero sized UDP packet towards the destination
-        socket_sendto ($send_socket, "", 0, 0, $dest_addr, $port);
+        socket_sendto($send_socket, "", 0, 0, $dest_addr, $port);
 
         // Wait for an event to occur on the socket or timeout after 5 seconds. This will take care of the
         // hanging when no data is received (packet is dropped silently for example)
-        $r = array ($recv_socket);
-        $w = $e = array ();
-        socket_select ($r, $w, $e, 5, 0);
+        $r = array($recv_socket);
+        $w = $e = array();
+        socket_select($r, $w, $e, 5, 0);
 
         // Nothing to read, which means a timeout has occurred.
-        if (count ($r)) {
+        if (count($r)) {
             // Receive data from socket (and fetch destination address from where this data was found)
-            socket_recvfrom ($recv_socket, $buf, 512, 0, $recv_addr, $recv_port);
+            socket_recvfrom($recv_socket, $buf, 512, 0, $recv_addr, $recv_port);
 
             // Calculate the roundtrip time
-            $roundtrip_time = ( microtime(true) – $t1 ) * 1000;
+	      $t2 = microtime(true);
+	      $roundtrip_time = ($t2 - $t1) * 1000;
 
             // No decent address found, display a * instead
             if (empty ($recv_addr)) {
@@ -49,14 +54,14 @@
                 $recv_name = "*";
             } else {
                 // Otherwise, fetch the hostname for the address found
-                $recv_name = gethostbyaddr ($recv_addr);
+                $recv_name = gethostbyaddr($recv_addr);
             }
 
             // Print statistics
-            printf ("%3d   %-15s  %.3f ms  %s\n", $ttl, $recv_addr,  $roundtrip_time, $recv_name);
+            printf ("%3d   %-15s  %.3f ms  %s<br>", $ttl, $recv_addr,  $roundtrip_time, $recv_name);
         } else {
             // A timeout has occurred, display a timeout
-            printf ("%3d   (timeout)\n", $ttl);
+            printf ("%3d   (timeout)<br>", $ttl);
         }
 
         // Close sockets
