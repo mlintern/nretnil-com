9c2a24102895fd4bcf4fb05e0bd06a7ec6dcd415
root <root@MLCLOUD.(none)>
2013-04-11 13:10:59 +0000

    updated file decode file

--- staging/navyprairiedogopenmap/misc/ruby/nokogiri_xml.rb
+++ staging/navyprairiedogopenmap/misc/ruby/nokogiri_xml.rb
@@ -2,16 +2,33 @@
 
 require 'rubygems'
 require 'nokogiri'
+require 'base64'
 
 icount = 0 
 xmlfeed = Nokogiri::XML(open("../partial.xml"))
 #puts xmlfeed
-all_items = xmlfeed.xpath("//update-log-entry")
+#all_items = xmlfeed.xpath("//update-log-entry")
+all_items = xmlfeed.xpath("//journal-entry")
 	all_items.each do |adv|
+		puts icount
+		if (adv.children.filter("title").first.child != nil)
 		puts "Title: " + adv.children.filter("title").first.child.inner_text
-		puts "Description: " + adv.children.filter("description").first.child.inner_text
+		end
+		if (adv.children.filter("url-id").first.child != nil)
+		puts "URL ID: " + adv.children.filter("url-id").first.child.inner_text
+		end
+		#if (adv.children.filter("description").first.child != nil)
+		#	puts "Description: " + adv.children.filter("description").first.child.inner_text
+		#end
+		if (adv.children.filter("body").first.child != nil)
+			plain = Base64.decode64(adv.children.filter("body").first.child.inner_text)
+			puts "Body: " + adv.children.filter("body").first.child.inner_text
+			puts "Body: " + plain
+		end
+		if (adv.children.filter("added-on").first.child != nil)
 		puts "Date: " + adv.children.filter("added-on").first.child.inner_text
-		puts adv
+		end
+		#puts adv
 		icount = icount + 1
 	end
 puts icount
