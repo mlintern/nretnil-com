abd93b12a1f08b7f8a10f83b1a20f56299659faf
Mark Lintern <mlintern@compendium.com>
2013-04-12 09:31:35 -0400

    added additional errors and help link

--- php/et-error.php
+++ php/et-error.php
@@ -16,6 +16,8 @@ case "8" : $strError = "Subscriber already exists."; break;
 case "9" : $strError = "An error has occurred while attempting to save your subscriber information."; break;
 case "10" : $strError = "An error has occurred while attempting to save your subscriber information."; break;
 case "12" : $strError = "The subscriber you are attempting to insert is on the master unsubscribe list or the global unsubscribe list."; break;
+case "13" : $strError = "Check that the list ID and/or MID specified in your code is correct."; break;
+case "14" : $strError = "The ET Subscriber key is not enabled."; break;
 default : $strError = "Other"; break;
 }
  
@@ -24,5 +26,6 @@ default : $strError = "Other"; break;
 </HEAD>
 <BODY>
 <?php echo 'Error: ('.$errorcode.') '.$strError; ?>
+<div>For More Information: <a href="http://help.exacttarget.com/en/documentation/exacttarget/subscribers/web_collect/">http://help.exacttarget.com/en/documentation/exacttarget/subscribers/web_collect/</a></div>
 </BODY>
 </HTML>
