1508a5be6cf2c9f794e7bd7447e50d760c99a36e
Mark Lintern <mlintern@compendium.com>
2013-04-17 13:44:18 -0400

    undo for test

--- staging/js/pinterest-hover-logo.js
+++ staging/js/pinterest-hover-logo.js
@@ -27,9 +27,9 @@ $(document).ready(function () {
         $(imageSelector).mouseenter(function () {
             $('.pinit-wrapper').css("visibility", "hidden");
             clearTimeout(bsButtonHover);
-            //$hoveredImg = $(this);
-            //media = $hoveredImg.prop('src');
-            if ( $(this).attr('src').toLowerCase().indexOf("http") >= 0 ){
+            $hoveredImg = $(this);
+            media = $hoveredImg.prop('src');
+            /*if ( $(this).attr('src').toLowerCase().indexOf("http") >= 0 ){
 	        	media = $(this).attr('src');
 	        }else{
 	        	if ( $(this).attr('src').toLowerCase().indexOf("//") >= 0 ){
@@ -37,7 +37,7 @@ $(document).ready(function () {
 	        	}else{
 	        	media = page_protocol + "//" + page_domain + "/" + $(this).attr('src');
 	        	}
-	        }
+	        }*/
             description = $hoveredImg.closest('.post').find(descriptionSelector).text();
             var pinitURL = $hoveredImg.closest('.post').find(urlSelector).attr('href');
             if (pinitURL == undefined) {
