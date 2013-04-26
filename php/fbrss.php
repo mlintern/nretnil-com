960d971eb75a60f900df36bd5f366acf616de3f4
root <root@MLCLOUD.(none)>
2013-04-26 21:53:17 +0000

    updated php file

--- php/fbrss.php
+++ php/fbrss.php
@@ -8,13 +8,8 @@ $fb = new facebook(array(
     'secret'=>  '8e2b8bc15c0a5e7b0c92255ba90235c2' // by registering an app
 ));
 
-$facebook_id = $_GET["id"];
 $page_name = $_GET["name"];
-if ( !empty($facebook_id ) ) {
-$response = $fb->api("/$facebook_id/feed",'GET'); // replace "spreetable" with your fb page name or username
-} else {
 $response = $fb->api("/$page_name/feed",'GET'); 
-}
 // create the feedwriter object (we're using ATOM but there're other options like rss, etc)
 $feed = new FeedWriter(ATOM);
 
@@ -23,7 +18,7 @@ $feed->setLink('http://nretnil.com/php/fbrss.php'); // set the url to the feed p
 
 $feed->setChannelElement('updated', date(DATE_ATOM , time()));
 $feed->setChannelElement('author', array('name'=>"$page_name")); // set the author name
-
+echo $response['data'];
 // iterate through the facebook response to add items to the feed
 foreach($response['data'] as $entry){
         if(isset($entry["message"])){
