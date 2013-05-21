7e6eecd044c47f5bac83953d0f4997d60d4dae79
Mark Lintern <markllintern@gmail.com>
2013-05-21 13:23:46 -0400

    slimmed down /ip/

--- ip/index.php
+++ ip/index.php
@@ -1,15 +1,4 @@
 <?php
 $client_ip = $_SERVER["REMOTE_ADDR"];
 ?>
-
-<!DOCTYPE HTML>
-
-<html>
-<head>
-<title>IP</title>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-</head>
-<body>
 <?php print $client_ip; ?>
-</body>
-</html>
