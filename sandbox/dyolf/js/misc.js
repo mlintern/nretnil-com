ff46a811c3aa4fa145a881c5b800683d70e7cf21
Mark Lintern <markllintern@gmail.com>
2013-10-22 16:00:10 -0400

    Added More to Floyd Site and update Fresh Start design

--- sandbox/dyolf/js/misc.js
+++ sandbox/dyolf/js/misc.js
@@ -17,5 +17,32 @@ $(document).ready(function() {
             	clearInterval(i);
             }, 500);
         }
-    }, 500)
+    }, 500);
+    
+    
+    //POPUP Form Code
+    var popupgo = function () {
+        var Box = '#floyd';
+        
+        // Add the mask to body
+        $('#mask').show();
+        //$('#mask').fadeIn(50);
+        
+        //Fade in the Popup
+        setTimeout(function(){$(Box).fadeIn(150);},400);
+        
+        return false;
+    };
+        
+    // When clicking on the button close or the mask layer the popup closed
+    $('#mask, #floyd').click(function() { 
+        $('#mask , #floyd').fadeOut(100 , function() {
+            $('#mask').hide();  
+        });
+    });
+    
+    $('.navbar-brand').click(function(){
+    	popupgo();
+    });
+    
 });
