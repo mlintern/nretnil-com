1434b6f81717e0a698612b443e529980c3a14d45
Mark Lintern <markllintern@gmail.com>
2013-12-16 17:41:01 -0500

    More Button Functionality

--- sandbox/sandybox/script.js
+++ sandbox/sandybox/script.js
@@ -2,6 +2,11 @@ $(document).ready(function() {
 	
 	$('#run').bind('click', function() {
 		runCode();
+		$('.preview-pane').css('width','100%');
+		$('.preview-pane').css('margin-left','0%');
+		$('.content').css('width','0%');
+		$('.btn-default').removeClass('btn-warning');
+		$('.all-display').addClass('btn-warning');
 	});
 
 	setTimeout(function(){runCode();},1000)
@@ -13,18 +18,24 @@ $(document).ready(function() {
 	$('.all-code').bind('click', function() {
 		$('.preview-pane').css('width','0%');
 		$('.content').css('width','100%');
+		$('.btn-default').removeClass('btn-warning');
+		$(this).addClass('btn-warning');
 	});
 	
 	$('.split').bind('click', function() {
 		$('.preview-pane').css('width','60%');
 		$('.preview-pane').css('margin-left','40%');
 		$('.content').css('width','40%');
+		$('.btn-default').removeClass('btn-warning');
+		$(this).addClass('btn-warning');
 	});
 	
 	$('.all-display').bind('click', function() {
 		$('.preview-pane').css('width','100%');
 		$('.preview-pane').css('margin-left','0%');
 		$('.content').css('width','0%');
+		$('.btn-default').removeClass('btn-warning');
+		$(this).addClass('btn-warning');
 	});
 	
 });
