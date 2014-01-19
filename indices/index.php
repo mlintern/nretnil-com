637df61f5e0bd64b0bf2a68923fe99a442f8ad3d
Mark Lintern <markllintern@gmail.com>
2014-01-19 17:04:00 -0500

    absolute path for secret script

--- indices/index.php
+++ indices/index.php
@@ -182,7 +182,7 @@
 </div><!-- /.modal -->
 
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-<script type="text/javascript" src="js/secret.js"></script>
+<script type="text/javascript" src="http://dev.nretnil.com/js/secret.js"></script>
 <script type="text/javascript" src="http://dev.nretnil.com/js/bootstrap3.js"></script>
 </body>
 </html>
