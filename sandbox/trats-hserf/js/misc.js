89f33042011dff3619b218c7415a2a15571f9689
Mark Lintern <markllintern@gmail.com>
2014-02-04 09:09:19 -0500

    Added thumbnail section

--- sandbox/trats-hserf/js/misc.js
+++ sandbox/trats-hserf/js/misc.js
@@ -8,16 +8,17 @@ $('.navbar li a.local').click(function(event) {
 
 
 $(document).ready(function() {
-    var i = setInterval(function(){
-        if($("#twitter-widget-0").length) {
-        	setTimeout(function(){ 
-        		$("#twitter-widget-0").width('100%');
-        		$("#twitter-widget-0").height('350');
-        		$("#twitter-widget-0").contents().find("body").append("<style type='text/css'> html, .p-author .profile .p-name, .cards-base p, .cards-base p a, .timeline-header .summary, .timeline-header .summary a:link, .timeline-header .summary a:visited, .p-author a.profile:hover .p-name, .p-author a.profile:focus .p-name {color: #cfcfcf;} .header .avatar {float:left;position:relative;left:0;margin-right: 5px;} .var-chromeless .tweet {padding:12px 2px 10px 2px;float:left;width:200px;margin-right:10px;color: #cfcfcf;} .var-static .stream {height: 100%;} .var-chromeless span.p-geo {padding: 0;} .tweet .e-entry-title {margin: 0;overflow: visible;} a:link, a:visited, .stats span, .byline, .retweet-credit, .no-more-pane, .no-tweets-pane p, .p-geo, .cards-base .byline-user {color: #aaaaaa;}</style>");
-            	clearInterval(i);
-            }, 500);
-        }
-    }, 500)
+
+	$('.minusOne').click(function(){
+		$(this).parents('.thumbnail').find('.rank').remove();
+		$(this).parents('.thumbnail').append('<div class="rank"><i class="fa fa-ban text-danger"></i></div>');
+	});
+	
+	$('.plusOne').click(function(){
+		$(this).parents('.thumbnail').find('.rank').remove();
+		$(this).parents('.thumbnail').append('<div class="rank"><i class="fa fa-check text-success"></i></div>')
+	});
+
 });
 
 function progressUp(id,amount) {
