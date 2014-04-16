3339994cfd7393b1d3d4c4e35c6270cc7d8a822b
Mark Lintern <markllintern@gmail.com>
2014-04-16 09:13:59 -0400

    dice: fixed custom characters setting
    updated javascript spacing
    removed cookie logging

--- sandbox/ecid/dice.js
+++ sandbox/ecid/dice.js
@@ -15,75 +15,62 @@ var die = function ( id ) {
 }
 
 die.prototype.roll = function () {
-  var self = this;
-  if (this.diceTimer) {
-    clearTimeout(this.diceTimer);
-  }
-  var ch = customLetters.charAt(Math.random() * customLetters.length);
-  if (option == 'dice') {
-  	img = '<span class="die die'+ch+'"></span>';
-  	$(".Dice[data-die="+this.id+"]").html(img);
-  	$(".Dice[data-die="+this.id+"]").data('value',ch);
-  } else {
-  	$(".Dice[data-die="+this.id+"]").html(ch);
-  	$(".Dice[data-die="+this.id+"]").data('value',ch);
-  }
-  switch (this.speedMode) {
-    case 0:
-      this.setDiceSpeed(this.diceSpeed - 25);
-      if (this.diceSpeed < 50) {
-        this.setDiceSpeed(50);
-        this.speedMode = SPEED.HOLD;
-      }
-      break;
-    case 1:
-      break;
-    case 2:
-      this.setDiceSpeed(this.diceSpeed + 10);
-      if (this.diceSpeed > 300) {
-        this.stopRolling(this.diceTimer);
-        return;
-      }
-      break;
-  }  
-  this.diceTimer = setTimeout(function(){self.roll()}, this.diceSpeed);
+	var self = this;
+	if (this.diceTimer) {
+		clearTimeout(this.diceTimer);
+	}
+	var ch = customLetters.charAt(Math.random() * customLetters.length);
+	if (option == 'dice') {
+		img = '<span class="die die'+ch+'"></span>';
+		$(".Dice[data-die="+this.id+"]").html(img);
+		$(".Dice[data-die="+this.id+"]").data('value',ch);
+	} else {
+		$(".Dice[data-die="+this.id+"]").html(ch);
+		$(".Dice[data-die="+this.id+"]").data('value',ch);
+	}
+	switch (this.speedMode) {
+	case 0:
+		this.setDiceSpeed(this.diceSpeed - 25);
+		if (this.diceSpeed < 50) {
+			this.setDiceSpeed(50);
+			this.speedMode = SPEED.HOLD;
+		}
+		break;
+	case 2:
+		this.setDiceSpeed(this.diceSpeed + 10);
+		if (this.diceSpeed > 300) {
+			this.stopRolling(this.diceTimer);
+			return;
+		}
+		break;
+	}  
+	this.diceTimer = setTimeout(function(){self.roll()}, this.diceSpeed);
 }
 
 die.prototype.startRoll = function () {
-  this.roll();
+	this.roll();
 }
 
 die.prototype.setDiceSpeed = function (speed) {
-  this.diceSpeed = speed;
+	this.diceSpeed = speed;
 }
 
 die.prototype.stopRoll = function () {
-  this.speedMode = SPEED.BRAKE;
+	this.speedMode = SPEED.BRAKE;
 }
 
 die.prototype.stopRolling = function () {
-  if (this.diceTimer) {
-    clearTimeout(this.diceTimer);
-  }      
-  this.setDiceSpeed(300);
-  this.speedMode = SPEED.HOLD;
-  this.diceTimer = null;
-  $(".RollHistory").text($(".RollHistory").text() + $(".Dice[data-die="+this.id+"]").data('value') + " ");
+	if (this.diceTimer) {
+		clearTimeout(this.diceTimer);
+	}      
+	this.setDiceSpeed(300);
+	this.speedMode = SPEED.HOLD;
+	this.diceTimer = null;
+	$(".RollHistory").text($(".RollHistory").text() + $(".Dice[data-die="+this.id+"]").data('value') + " ");
 }
 
 function setup() {
 	orientationChanged();
-	settingsCookie = new Cookie("DiceBox");
-	if (settingsCookie.get()) {
-		try {
-			var settings = eval("new Object({" + settingsCookie.get() + "})");
-			$(".CustomCheck").checked = settings.custom;
-			$(".CustomLetters").value = settings.letters;
-		}
-		catch (e) {
-			debug("LoadingSettings failed: " + e);
-		}  
-	}
 	customChanged();
 	moreDice(1);
 }
@@ -91,7 +78,6 @@ function setup() {
 function moreDice(num) {
 	for (i=0;i<num;i++) {
 		var name = new die(die_count);
-		dice.push(name);
 		$('.Content').append('<div class="DiceBox"><div class="Dice" data-die="'+die_count+'">&nbsp;</div></div>');
 		$('.Dice[data-die='+die_count+']').parent().on('click',function(){
 			if ( $(this).hasClass('lock') ) {
@@ -102,6 +88,7 @@ function moreDice(num) {
 				$(this).addClass('lock');
 			}
 		});
+		dice.push(name);
 		die_count++;
 	}
 }
@@ -131,18 +118,18 @@ function go() {
 }
 
 function clearHistory() {
-  $(".RollHistory").html("&nbsp;");
-  $(".RollHistory").text($(".RollHistory").text() + "- ");
+	$(".RollHistory").html("&nbsp;");
+	$(".RollHistory").text($(".RollHistory").text() + "- ");
 }
 
 function showSetup() {
-  $(".Setup").show();
-  $(".settings-open").hide();
+	$(".Setup").show();
+	$(".settings-open").hide();
 }
 
 function closeSetup() {
-  $(".Setup").hide();
-  $(".settings-open").show();
+	$(".Setup").hide();
+	$(".settings-open").show();
 }
 
 function customChanged() {
@@ -164,26 +151,19 @@ function customChanged() {
 			$(".CustomLetters").value = customLetters;
 			break;
 	} 
-	saveSettings();
-}
-
-function saveSettings() {
-  var text = "option:" + $("input:radio[name=DiceType]:checked").val();
-  text += ",letters:'" + $(".CustomLetters").val().replace(/\'/g, "") + "'";
-  settingsCookie.store(text);
 }
 
 function orientationChanged() {
-  if (window.orientation != undefined) {
-    landscape = window.orientation != 0 && window.orientation != 180;
-  }
-  else {
-    landscape = window.innerWidth > window.innerHeight;
-  }  
-  setTimeout(function() {window.scrollTo(0,1)}, 1);
+	if (window.orientation != undefined) {
+		landscape = window.orientation != 0 && window.orientation != 180;
+	}
+	else {
+		landscape = window.innerWidth > window.innerHeight;
+	}  
+	setTimeout(function() {window.scrollTo(0,1)}, 1);
 }
 
 function debug(msg) {
-  var e = $("Debug");
-  e.innerHTML += msg + "<br>";
+	var e = $("Debug");
+	e.innerHTML += msg + "<br>";
 }
