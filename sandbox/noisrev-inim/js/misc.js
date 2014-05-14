836a090efe52cb44eb5a64c53f9ea1ab8c6cb4f4
Mark Lintern <markllintern@gmail.com>
2014-05-14 09:49:37 -0400

    mini-sites page

---
+++ sandbox/noisrev-inim/js/misc.js
@@ -0,0 +1,28 @@
+jQuery.fn.showMiniSite = function(settings) {
+
+    var options = jQuery.extend({
+        site_width: '100%',
+        site_height: 'auto', // Set height based on width
+        disable_interation: true // Can click within the site windows
+    }, settings);
+
+    $(this).each(function() {
+        var site_url = $(this).data('site-url');
+        var div_width = $(this).width();
+        if (options.site_height == 'auto') {
+            var div_height = (div_width * 9) / 16;
+        }
+
+        $(this).append('<iframe src="'+site_url+'" height="'+div_height+'px" width="'+options.site_width+'"></iframe>');
+
+        if ( options.disable_interation ) {
+            $(this).css({'position': 'relative'});
+            $(this).append('<a href="'+site_url+'" style="position:absolute;top:0;bottom:0;left:0;right:0;"></a>')
+        }
+    })
+
+};
+
+$(document).ready(function(){
+    $('.mini-site').showMiniSite();
+})
