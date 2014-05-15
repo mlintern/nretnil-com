f4429dfc05aab5b1ed3485ae5df44e2f98f4d779
Mark Lintern <markllintern@gmail.com>
2014-05-15 11:19:43 -0400

    Removed uneeded jquery plugin code

--- sandbox/llorcs-on/js/misc.js
+++ sandbox/llorcs-on/js/misc.js
@@ -1,22 +1,3 @@
-jQuery.fn.removeInlineCss = function(property){
-
-    if(property == null)
-        return this.removeAttr('style');
-
-    var proporties = property.split(/\s+/);
-
-    return this.each(function(){
-        var remover = 
-            this.style.removeProperty   // modern browser
-            || this.style.removeAttribute   // old browser (ie 6-8)
-            || jQuery.noop;  //eventual
-
-        for(var i = 0 ; i < proporties.length ; i++)
-            remover.call(this.style,proporties[i]); 
-
-    });
-};
-
 jQuery.fn.zoomIn = function(settings){
 
     var options = jQuery.extend({
