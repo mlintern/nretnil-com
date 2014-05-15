f81ae3ef1ef93cdee66202d601ab4247a07ac0da
Mark Lintern <markllintern@gmail.com>
2014-05-15 18:13:33 -0400

    origin *

--- php/rss2xml.php
+++ php/rss2xml.php
@@ -3,6 +3,7 @@ if (isset($_GET["url"])) {
     $url = $_GET["url"];
     $xml = simplexml_load_file($url);
     header('Content-type: application/xml');
+    header('Access-Control-Allow-Origin: *');
     $xml = $xml->asXML();
 } else {
     $xml = 'Please send XML with request!  http://www.this.com/here.php?url=http://www.example.com/rss';
