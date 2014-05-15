ac6c1b1509b2c6776cc59cc91f3a374da084b26b
Mark Lintern <markllintern@gmail.com>
2014-05-15 18:01:48 -0400

    updates to php files

--- php/rss2xml.php
+++ php/rss2xml.php
@@ -1,12 +1,11 @@
 <?php
-
 if (isset($_GET["url"])) {
     $url = $_GET["url"];
-    $xml = simplexml_load_file('http://news.stanford.edu/rss/index.xml');
+    $xml = simplexml_load_file($url);
+    header('Content-type: application/xml');
+    $xml = $xml->asXML();
 } else {
-    $xml = 'Please send rss url with request!  http://www.this.com/here.php?url=http://www.example.com/rss';
+    $xml = 'Please send XML with request!  http://www.this.com/here.php?url=http://www.example.com/rss';
 }
-
 ?>
-
-<?php print $xml;?>
+<?php print $xml ?>
