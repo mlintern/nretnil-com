ef41ec41ce35025011a6205354af5dab66aed46b
Mark Lintern <markllintern@gmail.com>
2014-05-22 10:44:01 -0400

    Updated ZoomIn Plugin

--- staging/tools/js/jquery-plugins/zoomIn.jquery.js
+++ staging/tools/js/jquery-plugins/zoomIn.jquery.js
@@ -1,5 +1,5 @@
 /*
- * jQuery Zoom In plugin 1.0.1
+ * jQuery Zoom In plugin 1.1.0
  *
  * Mark Lintern
  *
@@ -7,13 +7,14 @@
  *   http://www.opensource.org/licenses/mit-license.php
  *   http://www.gnu.org/licenses/gpl.html
  *
- * Date: May 05 2014
+ * Date: May 22 2014
  */
 jQuery.fn.zoomIn = function(settings){
 
     var options = jQuery.extend({
         magnification: 2,
-        min_width: 768
+        min_width: 768,
+        direction: 'middle' // up, down, left, right, middle
     }, settings);
 
     if ($(window).width() >= options.min_width) {
@@ -21,21 +22,45 @@ jQuery.fn.zoomIn = function(settings){
         $(this).hover(function(){
 
             var magnification = options.magnification;
+            var direction = options.direction;
 
             if ($(this).data('magnification-times') != null){
                 magnification = $(this).data('magnification-times');
+            }
+            if ($(this).data('magnification-direction') != null){
+                direction = $(this).data('magnification-direction');
             }
 
             var cur_width = parseInt($(this).width());
             var cur_height = parseInt($(this).height());
             var top_margin = ((cur_height*magnification)/2)-(cur_height/2);
             var left_margin = ((cur_width*magnification)/2)-(cur_width/2);
-            console.log(cur_width*magnification);
             var styles = {
-                "margin-left": "-"+left_margin+"px",
-                "margin-top": "-"+top_margin+"px",
                 "width": (cur_width*magnification)+"px"
-            };
+            }
+
+            switch (direction) {
+                case 'up':
+                    styles['margin-left'] = '-'+left_margin+'px';
+                    styles['margin-top'] = '-'+(2*top_margin)+'px';
+                    break;
+                case 'down':
+                    styles['margin-left'] = '-'+left_margin+'px';
+                    styles['margin-bottom'] = '-'+(2*top_margin)+'px';
+                    break;
+                case 'left':
+                    styles['margin-left'] = '-'+(2*left_margin)+'px';
+                    styles['margin-top'] = '-'+top_margin+'px';
+                    break;
+                case 'right':
+                    styles['margin-right'] = '-'+(2*left_margin)+'px';
+                    styles['margin-top'] = '-'+top_margin+'px';
+                    break;
+                default:
+                    styles['margin-left'] = '-'+left_margin+'px';
+                    styles['margin-top'] = '-'+top_margin+'px';
+                    break;
+            }
             
             $(this).css( styles );
 
