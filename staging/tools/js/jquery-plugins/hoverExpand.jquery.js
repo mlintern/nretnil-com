d298d3fc3461a854db91df498cb449c95eb1c781
Mark Lintern <markllintern@gmail.com>
2014-09-11 10:38:23 -0400

    hover Expand updated functionality

--- staging/tools/js/jquery-plugins/hoverExpand.jquery.js
+++ staging/tools/js/jquery-plugins/hoverExpand.jquery.js
@@ -1,5 +1,5 @@
 /*
- * jQuery Hover Expand plugin 1.0.0
+ * jQuery Hover Expand plugin 1.1.0
  *
  * Mark Lintern
  *
@@ -7,25 +7,33 @@
  *   http://www.opensource.org/licenses/mit-license.php
  *   http://www.gnu.org/licenses/gpl.html
  *
- * Date: September 10, 2014
+ * Date: September 11, 2014
  */
 jQuery.fn.hoverExpand = function(settings){
 
     var options = jQuery.extend({
         delay: 500, // in milliseconds
         startHeight: 250, // in px
-        slideSpeed: 300, // in milliseconds
-        maxHeight: 100 // in %
+        slideSpeed: 500, // in milliseconds
+        maxHeight: 100, // in %
+        arrowHeight: 15, // in px
+        arrowWidth: 20 // in %
     }, settings);
     
     $(this).each( function () {
-
+        
         var divHeight = $(this).height();
         var expandedHeight = divHeight * (options.maxHeight/100);
         
         $(this).css({ 'overflow': "hidden" });
         
         if ( divHeight > options.startHeight ) {
+            
+                $(this).wrap( "<div class='arrow-container'></div>" );
+                var width = $(this).width();
+                var arrowMargin = width * ( ( 50 - ( options.arrowWidth / 2 ) ) / 100 );
+                var arrowSideBorder = width * ( ( options.arrowWidth / 2 ) / 100 );
+                $(this).parent('.arrow-container').append("<div class='arrow' style='border-top: solid "+options.arrowHeight+"px;border-left: solid "+arrowSideBorder+"px transparent;border-right: solid "+arrowSideBorder+"px transparent;margin: 5px "+arrowMargin+"px;'></div>");
                             
                 $(this).hover(function(){
             
@@ -33,12 +41,12 @@ jQuery.fn.hoverExpand = function(settings){
                     if ( options.maxHeight < 100 ) {
                         $(this).css({ 'overflow': "scroll" });
                     }
-                    $(this).animate({ 'height': expandedHeight },options.slideSpeed);
+                    $(this).animate({ 'height': expandedHeight },options.slideSpeed, function() { $(this).parent().find('.arrow').css('visibility','hidden'); });
             
                 },function(){
             
                     $(this).stop(true);
-                    $(this).animate({ 'height': options.startHeight },2*options.slideSpeed);
+                    $(this).animate({ 'height': options.startHeight },options.slideSpeed/2, function() { $(this).parent().find('.arrow').css('visibility','visible'); });
                     if ( options.maxHeight < 100 ) {
                         $(this).css({ 'overflow': "hidden" });
                         $(this).scrollTop(0);
