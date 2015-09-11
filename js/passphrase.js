bc2d2ab45894a5d28165a42cf2defa0bfd610ea9
Mark Lintern <mark.lintern@oracle.com>
2015-09-11 09:15:43 -0400

    capitalized pass phrase

--- js/passphrase.js
+++ js/passphrase.js
@@ -1,3 +1,7 @@
+function capitalize(s){
+	return s.toLowerCase().replace( /\b./g, function(a){ return a.toUpperCase(); } );
+};
+
 function passphrase_gen() {
 	$.ajax({
 		url: 'http://app.nretnil.com/api/password/phrase',
@@ -5,6 +9,7 @@ function passphrase_gen() {
 			console.log(response);
 			$("#passphrase_result").text(response['phonetic']);
 			$("#passphrase_result_nospace").text(response['password']);
+			$("#passphrase_result_caps").text(capitalize(response['phonetic']));
 		},
 		error: function(response) {
 			console.log(response);
