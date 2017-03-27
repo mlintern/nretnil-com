4c8101c15ad24d552f97126222d115576ed14755
Mark Lintern <mark.lintern@oracle.com>
2017-03-27 15:03:35 -0400

    updated date
     for new baby

--- sandbox/ybab/js/misc.js
+++ sandbox/ybab/js/misc.js
@@ -10,17 +10,17 @@ $('.navbar li a.local').click(function(event) {
 $(document).ready(function() {
     var i = setInterval(function(){
         if($("#twitter-widget-0").length) {
-        	setTimeout(function(){ 
+        	setTimeout(function(){
         		$("#twitter-widget-0").width('100%');
         		$("#twitter-widget-0").height('350');
         		$("#twitter-widget-0").contents().find("body").append("<style type='text/css'> html, .p-author .profile .p-name, .cards-base p, .cards-base p a, .timeline-header .summary, .timeline-header .summary a:link, .timeline-header .summary a:visited, .p-author a.profile:hover .p-name, .p-author a.profile:focus .p-name {color: #cfcfcf;} .header .avatar {float:left;position:relative;left:0;margin-right: 5px;} .var-chromeless .tweet {padding:12px 2px 10px 2px;float:left;width:200px;margin-right:10px;color: #cfcfcf;} .var-static .stream {height: 100%;} .var-chromeless span.p-geo {padding: 0;} .tweet .e-entry-title {margin: 0;overflow: visible;} a:link, a:visited, .stats span, .byline, .retweet-credit, .no-more-pane, .no-tweets-pane p, .p-geo, .cards-base .byline-user {color: #aaaaaa;}</style>");
             	clearInterval(i);
             }, 500);
         }
-    }, 500)
+    }, 500);
 
     $('#nav').localScroll(800);
-    
+
     //.parallax(xPosition, speedFactor, outerHeight) options:
     //xPosition - Horizontal position of the element
     //inertia - speed to move relative to vertical scroll. Example: 0.1 is one tenth the speed of scrolling, 2 is twice the speed of scrolling
@@ -33,7 +33,7 @@ $(document).ready(function() {
     $('.third .bg').parallax("50%", 0.4);
 
     $('.carousel').carousel('cycle');
-})
+});
 
-var endDate = new Date(2014,11,08,08,00,00,00,00);
+var endDate = new Date(2017,8,28,0,0,0,0,0);
 $('.countdown-div').countdown({until: endDate, pause: false});
