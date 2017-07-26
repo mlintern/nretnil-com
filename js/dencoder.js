0c6fa3fee45fa5d0b1cbf5cfcb8e23f162dca639
Mark Lintern <mark.lintern@oracle.com>
2017-07-26 11:22:46 -0400

    move to fontstrap

--- js/dencoder.js
+++ js/dencoder.js
@@ -1,16 +1,14 @@
 function encode() {
 	var text = $("#dencoder").val();
 	text = encodeURIComponent(text).replace(/'/g,"%27").replace(/"/g,"%22");
-	$('#dencoder-result').text(text);	
-	$('#dencoder-result').removeClass('hidden');
+	$('#dencoder-result').text(text);
 	$('#dencoder-result').focus();
-  	$('#dencoder-result').select();	
+	$('#dencoder-result').select();
 }
 function decode() {
 	var text = $("#dencoder").val();
 	text = decodeURIComponent(text.replace(/\+/g,  " "));
-	$('#dencoder-result').text(text);	
-	$('#dencoder-result').removeClass('hidden');
+	$('#dencoder-result').text(text);
 	$('#dencoder-result').focus();
-  	$('#dencoder-result').select();
+	$('#dencoder-result').select();
 }
