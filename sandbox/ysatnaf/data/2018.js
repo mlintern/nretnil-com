70de188c9dc0896951f6010b261d92c2194a81e2
Mark Lintern <mark.lintern@oracle.com>
2018-07-24 11:07:06 -0400

    prepped fantasy results for 2018

---
+++ sandbox/ysatnaf/data/2018.js
@@ -0,0 +1,77 @@
+var data2018 = {
+  'year': 2018,
+  'results': {
+    9: {
+      'name': 'Mark Lintern',
+      'team': 'R!3m@nn 5um',
+      'wins': 6,
+      'losses': 8
+    },
+    11: {
+      'name': 'Brian Konczos',
+      'team': 'Mr Rodgers Neighborhood',
+      'wins': 6,
+      'losses': 8
+    },
+    10: {
+      'name': 'Taylor Smith',
+      'team': 'Dream Team 2.0',
+      'wins': 4,
+      'losses': 10
+    },
+    3: {
+      'name': 'Brian Vehonsky',
+      'team': 'Kizer DeSöze',
+      'wins': 8,
+      'losses': 6
+    },
+    7: {
+      'name': 'Jeff Smith',
+      'team': 'Diggs out for Harambe',
+      'wins': 9,
+      'losses': 5
+    },
+    12: {
+      'name': 'Bobby Ritzi',
+      'team': 'Pile O Poo',
+      'wins': 6,
+      'losses': 8
+    },
+    8: {
+      'name': 'Andrew Huffman',
+      'team': 'Newark\'s Suburb Disappointment',
+      'wins': 3,
+      'losses': 11
+    },
+    4: {
+      'name': 'Sam Schmidt',
+      'team': 'Feed the Crow',
+      'wins': 8,
+      'losses': 6
+    },
+    5: {
+      'name': 'Steve Conard',
+      'team': 'The Super Mariota Bros',
+      'wins': 7,
+      'losses': 7
+    },
+    2: {
+      'name': 'Taylor Hafley',
+      'team': 'Gore on The Cobb',
+      'wins': 10,
+      'losses': 4
+    },
+    1: {
+      'name': 'Clint Pergram',
+      'team': 'Purple Helmut Soldiers',
+      'wins': 8,
+      'losses': 6
+    },
+    6: {
+      'name': 'Brian Desantis',
+      'team': 'DeSan\'s Cardiac Arrest',
+      'wins': 9,
+      'losses': 5
+    }
+  }
+};
