b70750bd09340ad32d8043b284a1c70bd6ab3063
Mark Lintern <mark.lintern@greenlight.guru>
2020-05-23 12:47:16 -0400

    small changes to nretnil

--- sandbox/dyolf/js/misc.js
+++ sandbox/dyolf/js/misc.js
@@ -1,34 +1,42 @@
 $(document).ready(function() {
+  var popupgo = function() {
+    var box = "#floyd";
 
-	var popupgo = function () {
-		var box = '#floyd';
+    // Add the mask to body
+    $("#mask").show();
 
-		// Add the mask to body
-		$('#mask').show();
+    //Fade in the Popup
+    setTimeout(function() {
+      $(box).fadeIn(150);
+    }, 400);
 
-		//Fade in the Popup
-		setTimeout(function(){$(box).fadeIn(150);},400);
+    return false;
+  };
 
-		return false;
-	};
+  // When clicking on the button close or the mask layer the popup closed
+  $("#mask, #floyd").click(function() {
+    $("#mask , #floyd").fadeOut(100, function() {
+      $("#mask").hide();
+    });
+  });
 
-	// When clicking on the button close or the mask layer the popup closed
-	$('#mask, #floyd').click(function() { 
-		$('#mask , #floyd').fadeOut(100 , function() {
-			$('#mask').hide();  
-		});
-	});
+  $(".go").click(function() {
+    popupgo();
+  });
 
-	$('.go').click(function(){
-		popupgo();
-	});
+  $(".lightbox").on("click", function(e) {
+    e.preventDefault();
+    var url =
+      window.location.href +
+      $(this)
+        .find("img")
+        .attr("src");
+    console.log(url);
 
-	$('.lightbox').on('click',function(e){
-		e.preventDefault();
-		var url = $(this).find('img').attr('src');
-		console.log(url);
-		$('.lightbox-modal').find('.modal-dialog .modal-body img').attr('src',url);
-		$('.lightbox-modal').modal('show');
-	})
-
+    $(".lightbox-modal")
+      .find(".modal-dialog .modal-body img")
+      .attr("src", url);
+    $(".lightbox-modal").modal("show");
+    $(".lightbox-modal").addClass("show");
+  });
 });
